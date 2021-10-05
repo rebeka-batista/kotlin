b0436cb4cfc64def30e252b3aecd480ba601ecfb
Rebeka Batista <rebekabatista05@outlook.com>
2021-10-05 16:49:59 -0300

    Generics

---
+++ src/main/kotlin/generics/Generics.kt
@@ -0,0 +1,14 @@
+package generics
+
+fun main() {
+    val strings = toList<String>("Rebeka", "Batista")
+    println(strings)
+    val ints = toList<Int>(1, 2, 3, 4, 5)
+    println(ints)
+}
+
+fun <T> toList(vararg args: T): List<T> {
+    val list = ArrayList<T>()
+    for (s in args) list.add(s)
+    return list
+}
