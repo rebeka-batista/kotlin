630a2013cad92206ba652ee6db2bd534dddbe74a
Rebeka Batista <rebekabatista05@outlook.com>
2021-10-06 14:55:59 -0300

    Higher-Order Functions e Lambdas

---
+++ src/main/kotlin/higherorderfunctions/HigherOrderFunctinos.kt
@@ -0,0 +1,33 @@
+package higherorderfunctions
+
+fun filtrar(list: List<Int>, filtro: (Int) -> (Boolean)): List<Int> {
+    val newList = arrayListOf<Int>()
+    for (item in list) {
+        if (filtro(item)) {
+            newList.add(item)
+        }
+    }
+    return newList
+}
+
+fun numerosPares(numero: Int) = numero % 2 == 0
+fun numerosMaiorQue3(numero: Int) = numero > 3
+
+
+/*
+      O Kotlin permite que funções recebam outras funções como parâmetro,
+      além de permitir que uma função retorne outra função (Higher-Order Functions)
+
+      A sintaxe ::funcao é utilizada para passar uma função como parâmetro, se a função
+      tiver os mesmos argumentos que a função desejada, a chamada será feita
+ */
+
+
+fun main() {
+    val ints = listOf(1, 2, 3, 4, 5)
+    println(ints)
+    var list = filtrar(ints, ::numerosPares)
+    println(list)
+    list = filtrar(ints, ::numerosMaiorQue3)
+    println(list)
+}
